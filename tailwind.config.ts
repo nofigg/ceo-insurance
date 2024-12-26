7a2407f3ef032e11d1118014b98945183a43e11c
Nolan Figueroa <nfigueroa06@gmail.com>
2024-12-25 18:18:53 -0700

    Add tailwind configuration file

---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'gradient-slow': 'gradient 15s ease infinite',
+      },
+      keyframes: {
+        gradient: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
