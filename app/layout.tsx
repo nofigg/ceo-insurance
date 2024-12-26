52f0aad96841266e19488aa59c9baaab1a4b727f
Nolan Figueroa <nfigueroa06@gmail.com>
2024-12-25 22:56:36 -0700

    fix: update SpeedInsights import path

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/react"
+import { SpeedInsights } from "@vercel/speed-insights"
 
 const inter = Inter({ subsets: ['latin'] })
 
